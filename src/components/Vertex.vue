c1840c47cfaa6ef52a7002dd8c25cdd1889fe960
ALONUCLEAR <alonrazka58@gmail.com>
2023-05-12 16:42:17 +0300

    feat(Vertex.vue): create vertex component

---
+++ src/components/Vertex.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <label> {{ shortestDistance }} </label> <br />
+    <svg :height="radius*3" :width="radius*3">
+        <circle :cx="x + radius * 1.5" :cy="y + radius * 1.5" :r="radius" stroke-width="3" :fill="color" />
+    </svg> <br />
+    <label> {{ name }} </label>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "VertexDisplay",
+    props: {
+        name: {
+            type: String,
+            default: "",
+        },
+        value: {
+            type: Number,
+            default: Infinity,
+        },
+        x: {
+            type: Number,
+            default: 0
+        },
+        y: {
+            type: Number,
+            default: 0
+        },
+        radius: {
+            type: Number,
+            default: 30
+        },
+        isExplored: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    computed: {
+        shortestDistance() {
+            return this.value < Infinity ? this.value : "∞";
+        },
+        color() {
+            return this.isExplored ? "green" : "red";
+        }
+    }
+}
+</script>
+
+<style scoped>
+circle {
+    stroke: black;
+}
+.dark circle {
+    stroke: white;
+}
+</style>
