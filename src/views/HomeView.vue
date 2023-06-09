1ea78e88054c468c6619d93fc56b7c1da9591612
ALONUCLEAR <alonrazka58@gmail.com>
2023-06-09 13:37:32 +0300

    feat(HomeView.vue): create an array of vertices and show them on screen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <vertex v-for="vertex in vertices" :key="vertex.name" :name="vertex.name" :isExplored="vertex.isExplored"
+    :x="vertex.x * unitScale" :y="vertex.y * unitScale" />
   </div>
 </template>
 
 <script>
+import Vertex from "@/components/Vertex.vue";
 
 export default {
-  name: 'HomeView',
-}
+  components: { Vertex },
+  name: "HomeView",
+  data() {
+    return {
+      unitScale: 5, //every unit in the video would be 20 * unitScale units here
+      vertices: [
+        { name: "A", isExplored: true, x: -60, y: 0 },
+        { name: "B", isExplored: false, x: 50, y: 10 },
+        { name: "C", isExplored: false, x: -30, y: 20 },
+        { name: "D", isExplored: false, x: 30, y: 40 },
+        { name: "E", isExplored: false, x: 0, y: 0 },
+        { name: "F", isExplored: false, x: -30, y: -20 },
+        { name: "G", isExplored: false, x: 30, y: -30 },
+      ],
+    };
+  },
+};
 </script>
